190f1d463733225d5711c4f3c78d1c703c454f91
au143 <you@example.com>
2024-03-01 18:18:47 +0500

    awsPush

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+  
+// })
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 5173 // Specify the desired port
+  }
 })
