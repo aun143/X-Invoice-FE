3b0022d466bbdf816fc42acb71b62d602fc4f9b9
au143 <you@example.com>
2024-03-01 17:47:32 +0500

    awsPush

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,5 @@
 // config.js
-export const BASE_URL = 'http://13.213.6.19:3010/api';
+// export const BASE_URL = 'http://13.213.6.19:3010/api';
+export const BASE_URL = 'http://3.1.100.174:3010/api';
 // export const BASE_URL = 'http://localhost:3010/api';
+// 
