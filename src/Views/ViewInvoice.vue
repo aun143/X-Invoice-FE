6665ba8cf05eb0e3758e8b627ee1c9263174c0c1
Muqeet Ahmad <inoxentuni1@gmaill.com>
2024-05-08 17:12:04 +0500

    pdfview

--- src/Views/ViewInvoice.vue
+++ src/Views/ViewInvoice.vue
@@ -145,7 +145,12 @@ const submitPassword = async () => {
                   <form @submit.prevent="submitPassword" class="space-y-4">
                       <div class="relative">
                           <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                          <input v-model="password" type="number" name="password" id="password" placeholder="" class="bg- border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-10 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                         <div class="bg-white">
+                          <input v-model="password" type="number" name="password" id="password" placeholder="Enter Your Password" class="input text-black w-full rounded-lg ">
+
+                          <!-- <input v-model="password" type="number" name="password" id="password" placeholder="Enter Your Password" class="input text-black"> -->
+                         </div>
+                          <!-- <input v-model="password" type="number" name="password" id="password" placeholder="" class="bg- border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-10 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required /> -->
                       </div>
                       <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
                   </form>
